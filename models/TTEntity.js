e684a13b0c332931a29e31b45491808b4b1f05dd
Huang Weilong <39356384+tracetoio-weilong@users.noreply.github.com>
2019-01-21 12:21:34 +0800

    Feature/239 web3 callback issue provider issue (#8)
    * Add http provider for verifier
    
    * Add http provider for approve
    
    * Update for package-lock 

--- models/TTEntity.js
+++ models/TTEntity.js
@@ -10,16 +10,20 @@ class TTEntity {
       case 'dev':
       case 'beta':
         this.provider = 'wss://ropsten.infura.io/ws';
+        this.httpProvider = 'https://ropsten.infura.io/'+process.env.INFURA_KEY;
         break;
       case 'app':
       default:
         this.provider = 'wss://mainnet.infura.io/ws';
+        this.httpProvider = 'https://mainnet.infura.io/'+process.env.INFURA_KEY;
     }
+    this.priKey = priKey;
+    
     this.w3 = new tracetoWeb3(this.provider);
     this.w3.setWallet(priKey);
+
     this.t2tContract = this.w3.addContract('t2t', environment.TraceToToken.address, environment.TraceToToken.abi);
     this.gasPrice = 1;
-    
   }
   getName(){
     return this.name;
@@ -31,7 +35,11 @@ class TTEntity {
     this.gasPrice = _gasPrice;
   }
   approveToken(receiver, amount, callback) {
-    return this.w3.sendToContractbyIdx(this.t2tContract, 'approve', this.gasPrice*4, callback, receiver, amount);
+    //Creating a new instance of httpw3 provider to ensure that there is a connection.
+    const httpw3 = new tracetoWeb3(this.httpProvider);
+    httpw3.setWallet(this.priKey);
+    const t2tContract = httpw3.addContract('t2t', environment.TraceToToken.address, environment.TraceToToken.abi);
+    return httpw3.sendToContractbyIdx(t2tContract, 'approve', this.gasPrice*4, callback, receiver, amount);
   }
 }
 
