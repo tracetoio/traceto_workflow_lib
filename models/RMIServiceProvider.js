f169e11f129c2ca8d58cab54ddb3fa5f2bd4662d
Huang Weilong <39356384+tracetoio-weilong@users.noreply.github.com>
2019-07-10 10:57:53 +0800

    Develop (#16)
    * 257 upgraded infuratoken
    
    * Refactor to a slimmer version
    
    * update package-json

--- models/RMIServiceProvider.js
+++ models/RMIServiceProvider.js
@@ -9,29 +9,16 @@ class RMIServiceProvider extends TTEntity {
     this.ProfileContract = this.w3.addContract('ProfileToken', environment.TraceToProfileToken.address, environment.TraceToProfileToken.abi);
   }
   addResultContract(name, addr){
-    const _this = this;
     return this.w3.addContract(name, addr, environment.TraceToProfileResult.abi);
   }
   getPubKey(contractIdx){
-    const _this = this;
-    return new Promise(function(resolve, reject) {
-      _this.w3.callContractbyIdx(contractIdx, 'getPubKey')
-      .then(data => resolve(data), reason => reject(reason));
-    });
+    return this.w3.callContractbyIdx(contractIdx, 'getPubKey');
   }
   setResultForProfile(contractIdx, profileId, result, decay, expire){
-    const _this = this;
-    return new Promise(function(resolve, reject) {
-      _this.w3.sendToContractbyIdx(contractIdx, 'setRMIResult', _this.gasPrice*4, profileId, result, decay, expire)
-      .then(data => resolve(data), reason => reject(reason));
-    });
+    return this.w3.sendToContractbyIdx(contractIdx, 'setRMIResult', this.gasPrice*4, profileId, result, decay, expire);
   }
   getProfile(profileId){
-    const _this = this;
-    return new Promise(function(resolve, reject) {
-      _this.w3.callContractbyIdx(_this.ProfileContract, 'getProfile', profileId)
-      .then(data => resolve(data), reason => reject(reason));
-    });
+    return this.w3.callContractbyIdx(this.ProfileContract, 'getProfile', profileId);
   }
   getPendingEvents(fromBlock='latest'){
     return this.w3.getAllContractEventbyId(this.ServiceCreditContract, fromBlock);
--- models/ToBeRequestor.js
+++ models/ToBeRequestor.js
@@ -8,22 +8,14 @@ class ToBeRequestor extends TTEntity {
     this.RequestorList = this.w3.addContract('RequestorList', environment.TraceToRequestorList.address, environment.TraceToRequestorList.abi);
   }
   apply(country, name, email, uriForMoreDetails, hashForMoreDetails){
-    const _this = this;
-    return new Promise(function(resolve, reject) {
-      _this.w3.sendToContractbyIdx(_this.RequestorList, 'addPendingRequestorPR', _this.gasPrice*4, _this.w3.getWalletAddress(), country, name, email, uriForMoreDetails, hashForMoreDetails)
-        .then(data => resolve(data), reason => reject(reason));
-    });
+    return this.w3.sendToContractbyIdx(this.RequestorList, 'addPendingRequestorPR', this.gasPrice*4, this.w3.getWalletAddress(), country, name, email, uriForMoreDetails, hashForMoreDetails);
   }
   getStatus(){
-    const _this = this;
-    return new Promise(function(resolve, reject) {
-      _this.w3.callContractbyIdx(_this.RequestorList, 'isRequestorPR', _this.w3.getWalletAddress())
-      .then(data => resolve(data), reason => reject(reason));
-    });
+    return this.w3.callContractbyIdx(this.RequestorList, 'isRequestorPR', this.w3.getWalletAddress());
   }
 
   getRequestorEvents(fromBlock='latest'){
-    return this.w3.getAllContractEventbyId(_this.RequestorList, fromBlock);
+    return this.w3.getAllContractEventbyId(this.RequestorList, fromBlock);
   }
 }
 
